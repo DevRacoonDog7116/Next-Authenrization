43504be295b3893ea649c87e771a4ff66bf15faf
S Future <tiaguitov08@gmail.com>
2025-04-21 22:08:01 +0900

    Update use-user.tsx

--- src/client/use-user.tsx
+++ src/client/use-user.tsx
@@ -9,15 +9,15 @@ import ConfigProvider, { ConfigContext } from './use-config';
  * @category Client
  */
 export interface UserProfile {
-  email?: string | null;
-  email_verified?: boolean | null; 
-  name?: string | null; 
+  email?: string | null; 
+  email_verified?: boolean | null;
+  name?: string | null;
   nickname?: string | null;
-  picture?: string | null;  
-  sub?: string | null; 
-  updated_at?: string | null; 
-  org_id?: string | null; 
-  [key: string]: unknown; // Any custom claim which could be in the profile  
+  picture?: string | null;
+  sub?: string | null;
+  updated_at?: string | null;
+  org_id?: string | null;
+  [key: string]: unknown; // Any custom claim which could be in the profile
 }
 
 /**
